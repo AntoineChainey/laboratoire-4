1a8d241da5b4a5416abde0ec20558f48d1e2d303
AntoineChainey <202487791@cegepsherbrooke.qc.ca>
2025-02-12 15:03:42 -0500

    Update excercise prog.py

--- excercise prog.py
+++ excercise prog.py
@@ -43,18 +43,18 @@ while test1 == 0 :      #boucle pour recommencer quand la réponse est incorrect
             rythmeconsommation = input("a quel fréquence consommer vous: ")     
             if rythmeconsommation == "chaque jour":                             
                 print("merci pour votre réponse")
-                test1 = 1
+                break
             elif rythmeconsommation == "chaque semaine":
                 print("merci pour votre réponse")
-                test1 = 1
+                break
             elif rythmeconsommation == "chaque mois" or rythmeconsommation == "autre":
                 print("bravo merci pour votre réponse")
-                test1 = 1
+                break
             else:
                 print("réponse incorect")        
         elif consommation == "non":
             print("bravo") 
-            test1 = 1
+            break
         else:
             print("réponse incorrect")
     elif age >= 30 and age < 50 :
@@ -101,3 +101,84 @@ while test1 == 0 :      #boucle pour recommencer quand la réponse est incorrect
             print("réponse incorrect")
     else :
         print("réponse incorect")
+personne = [age, consommation, rythmeconsommation ]  #repertorie les information recueillis plius haut
+
+with open("mon_fichier.txt", "a") as fichier:
+    fichier.write(f"Age: {age}, Consommation: {consommation}, Rythme: {rythmeconsommation}\n") # ouvre u n fichier texte et y inscris les information selon leur classe
+
+print("les informations ont été reccueilis.")
+
+b = True                                        #définis la variable
+while b == True :       #boucle du deuxieme questionnaire
+    continuer = input("voulez vous continuer: ")            #demande si l'utilisateur veut passer au deuxieme questionnaire
+    if continuer == "oui" :                     #condition pour savoir si l'utilisateur continue
+        consommation_drogue = input("consommer vous de la drogue: ")  # demande si l'utilisateur consomme de la drogue
+        if consommation_drogue == "oui" :
+            a=5
+            for i in range(a) :                 # boucle qui permet de répété la question si la réponse est invallide
+                fréquence_drogue = input("consommez-vous chaque semaine: ") #demande si l'utilisateur consomme chaque semaine a condition que la réponse précédente sois oui
+                if fréquence_drogue == "oui" :
+                    print("vous consommez chaque semaine")
+                    break                   # renvoie a la fin du quiz
+                elif fréquence_drogue == "non" :
+                    print("vous ne consommez pas chaque semaine")
+                    continue        #recommence la boucle.
+                else :
+                    print("réponse incorrect")
+                    continue
+            b = False
+            c = 5    
+            for i in range(c) :  #la boucle ce répete pour toutes les valeurs comprise dans le range c
+                fréquence_drogue = input("consommez vous chaque mois: ")    
+                if fréquence_drogue == "oui" :
+                    print("vous consommez chaque mois")
+                    break
+                elif fréquence_drogue == "non" :
+                    print("vous ne consommez pas chaque mois")
+                    d = 5    
+                    for i in range(d) :   
+                        fréquence_drogue = input("vous consommez a une autre fréquence que celle proposer: ")
+                        if fréquence_drogue == "oui" :
+                            fréquence_drogue = input("a quel fréquence consommer vous: ")
+                            break
+                        elif fréquence_drogue == "non" :
+                            print("vous avez répondue non le questionnaire recommence.")
+                            continue
+                        else :
+                            print("réponse invallide")
+                            continue
+            b = False
+                else :
+                    print("réponse incorrect")
+                    continue
+            b = False
+            d = 5    
+            for i in range(d) :   
+                fréquence_drogue = input("vous consommez a une autre fréquence que celle proposer: ")
+                if fréquence_drogue == "oui" :
+                    fréquence_drogue = input("a quel fréquence consommer vous: ")
+                    break
+                    
+                elif fréquence_drogue == "non" :
+                    print("vous avez répondue non le questionnaire recommence.")
+                    continue
+                else :
+                    print("réponse invallide")
+                    continue
+            b = False
+        elif consommation_drogue == "non" :
+            print("vous ne consommez pas de drogue")
+            b = False
+        else :
+            print("réponse incorrect")
+    elif continuer == "non" :
+        print("le test est finis")
+        b = False
+    else :
+        print("réponse incorrect")
+        continue
+
+
+        
+
+
